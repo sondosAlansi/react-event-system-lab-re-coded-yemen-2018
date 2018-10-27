b4b259bf2b358f22aa1ebf43614aa9dba822ca58
sondos alansi <sondos.alansi@yahoo.com>
2018-10-27 21:14:38 +0000

    automatically backed up by learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+export default class EyesOnMe extends React.Component{
+  fo = () => {
+      console.log('Good!');
+    }
+    bl = () => {
+        console.log('Hey! Eyes on me!');
+      }
+render(){
+
+
+  return(
+
+    <button onfocus={this.fo}  onblur={this.bl}>click</button>
+  )
+}
+
+}
+
