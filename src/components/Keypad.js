d612e8821c81037aa2c9ea45f2dccb34f8a2649a
sondos alansi <sondos.alansi@yahoo.com>
2018-10-27 21:09:28 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React from 'react';
+
+export default class Keypad extends React.Component{
+  enter = () => {
+      console.log('Entering password...');
+    }
+render(){
+
+
+  return(
+
+    <input type="password" onkeyUp={this.enter} />
+  )
+}
+  
+}
